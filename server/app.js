8b180dcc6c82a77eaa726b5149e1bab103afb131
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-09-19 04:57:25 +0530

    server side done

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const cros = require("cors");
 const requestIp = require("request-ip");
 const mongoos = require("mongoose");
 require("dotenv").config();
+const UAParser = require("ua-parser-js");
+const User = require("./model/User");
 
 const MONGO_CONNECT = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}mymongoinit.6md0cxy.mongodb.net/safu?retryWrites=true&w=majority`;
 const PORT = "3030";
@@ -13,6 +15,41 @@ const publicRoutes = require("./routes/public");
 app.use(express.json());
 app.use(cros());
 app.use(requestIp.mw());
+
+app.use((req, res, next) => {
+  User.findOne({ ip: req.clientIp })
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        console.log("alredy exist");
+        next();
+      } else {
+        const useragent = req.headers["user-agent"];
+        let parser = new UAParser(useragent);
+        let parserResults = parser.getResult();
+        const newUser = new User({
+          ip: req.clientIp,
+          deviceDetails: parserResults,
+          query: [],
+        });
+
+        return newUser.save().then((result) => {
+          User.findOne({ ip: req.clientIp })
+            .then((user) => {
+              req.user = user;
+              next();
+              console.log("new user");
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use(publicRoutes);
 
--- server/controller/public.js
+++ server/controller/public.js
@@ -1,3 +1,60 @@
+const axios = require("axios");
+
 exports.getApi = (req, res, next) => {
   res.status(200).json({ message: "Welcome To Safu API V1" });
+};
+
+exports.postApi = (req, res, next) => {
+  const url = req.budy.url;
+
+  const options = {
+    method: "GET",
+    url: "https://article-data-extraction-and-summarization.p.rapidapi.com/article",
+    params: {
+      url: url,
+      summarize: "true",
+      summarize_language: "auto",
+    },
+    headers: {
+      "X-RapidAPI-Key": process.env.API_KEY,
+      "X-RapidAPI-Host":
+        "article-data-extraction-and-summarization.p.rapidapi.com",
+    },
+  };
+
+  try {
+    axios
+      .request(options)
+      .then((response) => {
+        const summary = response.data.article.summary;
+        res.status(200).jason({ summary: summary });
+
+        return req.user
+          .addQuery({ question: url, summary: summary })
+          .then((result) => {
+            console.log(result);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          status: "fail",
+          error: "An unexpected error occurred. Please try again later.",
+          code: 500,
+        });
+        const err = new Error(error);
+        err.httpStatusCode = 500;
+        return next(err);
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "fail",
+      error: "An unexpected error occurred. Please try again later.",
+      code: 500,
+    });
+    const err = new Error(error);
+    err.httpStatusCode = 500;
+    return next(err);
+  }
 };
