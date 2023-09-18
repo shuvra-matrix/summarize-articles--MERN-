f9f7556cf7097845bb89d1ca243b854ebd60b1f4
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-09-19 03:47:55 +0530

    mobile responsive done

--- public/src/components/UserInput/InputSection.js
+++ public/src/components/UserInput/InputSection.js
@@ -6,15 +6,11 @@ const InputSection = () => {
       <input type="text" placeholder="Past Link Here"></input>
       <img
         className={styles.link}
-        width="40"
-        height="40"
         src="https://img.icons8.com/color/48/000000/link--v1.png"
         alt="link--v1"
       />
       <button type="submit">
         <img
-          width="40"
-          height="40"
           src="https://img.icons8.com/external-kmg-design-flat-kmg-design/64/000000/external-send-user-interface-kmg-design-flat-kmg-design.png"
           alt="external-send-user-interface-kmg-design-flat-kmg-design"
         />
