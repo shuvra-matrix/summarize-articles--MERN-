f37120a50a3f706544ae5a5c93af16fe413aa1e7
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-09-19 02:49:24 +0530

    input section and result section design done

--- public/src/components/UserInput/UserInput.js
+++ public/src/components/UserInput/UserInput.js
@@ -1,10 +1,12 @@
 import InputSection from "./InputSection";
+import ResultSection from "./ResultSection";
 import styles from "./UserInput.module.css";
 
 const UserInput = () => {
   return (
     <div className={styles["userinput"]}>
       <InputSection />
+      <ResultSection />
     </div>
   );
 };
