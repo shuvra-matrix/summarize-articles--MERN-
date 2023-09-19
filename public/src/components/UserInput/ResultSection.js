f55ab878af92479ead4134ccdb6dbc7587201b6a
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-09-19 08:59:57 +0530

    add copy result option and change icon

--- public/src/components/UserInput/ResultSection.js
+++ public/src/components/UserInput/ResultSection.js
@@ -1,7 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import styles from "./ResultSection.module.css";
 
 const ResultSection = (props) => {
+  const [isCopy, setCopy] = useState(false);
+
+  const copyHandler = (e) => {
+    navigator.clipboard.writeText(props.result.summary);
+    setCopy(true);
+    setTimeout(() => setCopy(false), 1000);
+  };
+
   return (
     <Fragment>
       <h3 className={styles["article-heading"]}>
@@ -9,6 +17,12 @@ const ResultSection = (props) => {
       </h3>
       <div className={styles["result-section"]}>
         <p>{props.result.summary}</p>
+        <img
+          onClick={copyHandler}
+          src="https://img.icons8.com/fluency/48/copy.png"
+          alt="copy"
+        />
+        {isCopy && <p className={styles["copy-message"]}>Copy</p>}
       </div>
     </Fragment>
   );
